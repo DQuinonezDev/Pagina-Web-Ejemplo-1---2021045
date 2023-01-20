721d4641e510ca77059984839c9fb1fa9355cc17
David Quiñonez <dquinonez-2021045@kinal.edu.gt>
2023-01-20 09:04:57 -0600

    Este es el primer commit de este proyecto

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+ 
+function suma(){
+
+    let num1 = document.getElementById("no1").value
+    let num2 = document.getElementById("no2").value
+
+    let resultado = parseInt(num1) + parseInt(num2)
+
+    console.log(resultado)
+
+}   
